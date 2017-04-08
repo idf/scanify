71f94466f225a0c70af9caf4e272c7e3468d65c2
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2017-04-08 13:12:17 -0700

    init

---
+++ scanify.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""scanify
+
+Usage:
+  scanify <path>
+  scanify <path> -o <outpath>
+  scanify -e <path>
+Examples:
+  scanify '~/Desktop/p0.jpg'
+  # output to '~/Desktop/p'~/Desktop/p0_scanify.jpg'
+
+Options:
+  -o --out      output path of the image, default output to the same path with
+                sufixx
+  -e --exposure adjust image exposreu
+"""
+
+from scanify import core
+from docopt import docopt
+
+
+if __name__ == "__main__":
+    runnable = core.Scanifier()
+    options = docopt(__doc__, version='scanify 0.0.1')
+    context_dict = {}
+    in_path = options['<path>']
+    out_path = options.get('<outpath>')
+    increase_exposure = True if options['--exposure'] else False
+
+    runnable.run(in_path, out_path, increase_exposure)
---
+++ scanify/core.py
@@ -0,0 +1,51 @@
+import os
+
+import numpy as np
+from scipy import misc, ndimage
+from skimage import exposure
+
+__author__ = 'Daniel'
+
+
+class Scanifier(object):
+    def run(self, imgin_path, imgout_path=None, increase_exposure=False):
+        imgin_path = self.__expand_user(imgin_path)
+        img = misc.imread(imgin_path)
+
+        img_blurred = self.__blur(img)
+        img = self.__divide(img, img_blurred)
+        if increase_exposure:
+            img = exposure.adjust_sigmoid(img)
+
+        if not imgout_path:
+            imgout_path = self.__add_suffix(imgin_path)
+        misc.imsave(imgout_path, img)
+
+    def __expand_user(self, path):
+        if path.startswith('~/'):
+            return os.path.join(os.path.expanduser('~'), path[2:])
+        return path
+
+    def __add_suffix(self, path):
+        name, ext = os.path.splitext(path)
+        return "{name}_{suffix}{ext}".format(name=name, suffix="scanify",
+                                          ext=ext)
+
+    def __blur(self, img):
+        """
+        Gaussian blur
+        """
+        return ndimage.gaussian_filter(img, sigma=100)
+
+    def __divide(self, a, b):
+        """
+        divide blend
+        """
+        c = a / ((b.astype('float') + 1) / 256)
+        d = c * (c < 255) + 255 * np.ones(np.shape(c)) * (c > 255)
+        e = d.astype('uint8')
+        return e
+
+if __name__ == "__main__":
+    scanifier = Scanifier()
+    ret1 = scanifier.run('~/Desktop/p0.jpg')
